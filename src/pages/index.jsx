e2ce35035158e453dec9de43e311bbf76c697147
Duuuuardo <79720101+Duuuuardo@users.noreply.github.com>
2024-05-28 14:28:10 -0300

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,7 @@ import MainPage from "../components/pages/MainPage";
 
 export default function Home() {
   return (
-    <main className="px-[20%]">
+    <main className="px-[20%] bg-black">
       <MainPage />
       <div className="flicker" />
       <div className="scanlines" />
