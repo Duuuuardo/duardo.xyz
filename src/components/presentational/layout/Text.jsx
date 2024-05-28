1919797190bbb14cd5108ae3b9263ae860ab7d17
Duuuuardo <79720101+Duuuuardo@users.noreply.github.com>
2024-05-28 14:31:31 -0300

    Update Text.jsx

--- src/components/presentational/layout/Text.jsx
+++ src/components/presentational/layout/Text.jsx
@@ -1,9 +1,9 @@
 function TextComponent({ content, title, position }) {
   return (
-    <div className="flex max-w-xl items-start flex-col">
+    <div className="flex max-w-xl items-start flex-col text-white">
       <div>
-        <h1 className={`text-4xl mb-3 font-bold ${position}`}>{title}</h1>
-        <div className={`text-xl mb-1 ${position}`}>{content}</div>
+        <h1 className={`text-4xl mb-3 text-white font-bold ${position}`}>{title}</h1>
+        <div className={`text-xl mb-1 text-white ${position}`}>{content}</div>
       </div>
     </div>
   );
